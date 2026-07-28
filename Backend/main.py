c143144f25852095940046427bbfc13e3a9df279
Uchenna King <theodoreking100@gmail.com>
2026-07-28 15:30:21 +0100

    Added a FastAPI webhook listener and logging

---
+++ Backend/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import logging
+import requests 
+import psutil
+app = FastAPI()
+
+
+def setup_logging():
+    logger = logging.getLogger("WebhookMonitor")
+    logging.basicConfig(level=logging.INFO)
+    return logger
+
+def check_external_service() -> bool:
+    try:
+        response = requests.get("http://127.0.0.1:8000", timeout=2)
+        return response.status_code == 200
+    except requests.exceptions.RequestException as err:
+        print(err)
+        return False
+
+def get_performance_metrics() -> dict:
+    return {
+        "cpu performance" : psutil.cpu_percent(1),
+        "memory percentage" : psutil.virtual_memory().percent,
+    }
+
+@app.get("/")
+def read_root() -> dict:
+    return {"message" : "server is running"}
+
+@app.get("/health")
+def health() -> JSONResponse:
+    external_ok = check_external_service()
+    metrics = get_performance_metrics()
+    return JSONResponse(
+        content = {
+            "status": "healthy" if external_ok else "unhealthy",
+            "external_service": external_ok,
+            "metrics": metrics,
+        },
+
+        status_code=200 if external_ok else 503,
+    )
+
+@app.post("/webhook")
+async def webhook_listener(request: Request):
+    monitor = setup_logging()
+    headers = dict(request.headers)
+    payload =  await request.json()
+    monitor.info("Received webhook with headers: %s", headers)
+    monitor.info("Received webhook data: %s", payload)
+    return {"status" : "received"}
